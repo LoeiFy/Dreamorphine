206d622058cfa80088f9e2482b777aa4c3eaab48
LoeiFy <loeify@gmail.com>
2015-12-06 10:02:18 +0800

    add CBFimage

--- assets/Dreamorphine.js
+++ assets/Dreamorphine.js
@@ -29,12 +29,12 @@ $(function($) {
     for (var i = 0; i < covers.length; i ++) {
         
         // random left, top
-        var w = container.width(), h = 300, _w = 200, _h = 200, t = 0, l = 0;
+        var w = container.width(), h = 400, _w = 200, _h = 200, t = 0, l = 0;
 
         t = R(- _h / 8, h - _h / 8 * 7);
 
         if (i % 3 === 0) {
-            l = R(_w / 8, w / 3 - _w / 8 * 7)
+            l = R(0, w / 3 - _w / 8 * 7)
         }
 
         if (i % 3 === 1) {
@@ -42,7 +42,7 @@ $(function($) {
         }
 
         if (i % 3 === 2) {
-            l = R(- _w / 8, w / 3 - _w / 8 * 9)
+            l = R(- _w / 8, w / 3 - _w)
         }
 
         str += '<img style="margin-top:'+ t +'px;margin-left:'+ l +'px" src="thumbnails/'+ covers[i][0] +'.jpg" data-c="'+ covers[i][1] +'" data-w="'+ covers[i][2] +'" data-h="'+ covers[i][3] +'" data-m="'+ covers[i][4] +'" data-r="'+ covers[i][5] +'" />';
@@ -88,3 +88,138 @@ $(function($) {
     })
 
 })
+
+/*
+ * https://github.com/LoeiFy/CBFimage/blob/master/README.md
+ *
+ * @version 1.0.0
+ * @author LoeiFy@gmail.com
+ * http://lorem.in/ | under MIT license
+ */
+
+;(function(window, undefined) {
+
+    function CBFimage(element, option) {
+
+        this.option = {
+            start:      function() {},
+            progress:   function(loaded, total) {},
+            complete:   function(image) {}
+        }
+
+        if (option && typeof option === 'object') {
+            for (var key in option) {
+                this.option[key] = option[key]
+            }
+        }
+
+        if (element && element.getAttribute('src') !== '') {
+            this.element = element;
+
+            this.src = element.getAttribute('src');
+
+            var blur = parseInt(element.getAttribute('blur'));
+            this.blur = blur > 0 && blur <= 10 ? blur : 0;
+
+            this.loadImg()
+        }
+
+    }
+
+    // canvas draw image
+    CBFimage.prototype.canvasImg = function(image) {
+
+        this.element.width = image.width;
+        this.element.height = image.height;
+
+        var context = this.element.getContext('2d');
+        context.drawImage(image, 0, 0)
+
+        if (this.blur > 0) {
+            context.globalAlpha = 0.5;
+
+            for (var y = -this.blur; y <= this.blur; y += 2) {
+                for (var x = -this.blur; x <= this.blur; x += 2) {
+                    context.drawImage(this.element, x + 1, y + 1)
+                    if (x >= 0 && y >= 0) {
+                        context.drawImage(this.element, -(x-1), -(y-1))
+                    }
+                }
+            }
+            context.globalAlpha = 1;
+        }
+
+    }
+
+    // xhr get image
+    CBFimage.prototype.loadImg = function () {
+
+        var request = new XMLHttpRequest(),
+            that = this;
+
+        request.onloadstart = function() {
+            that.option.start()
+        }
+
+        request.onprogress = function(e) {
+            // may total = 0
+            if (parseInt(e.total) !== 0) {
+                that.option.progress(e.loaded, e.total)
+            }
+        }
+
+        request.onload = function(e) {
+            if (this.status >= 200 && this.status < 400) {
+                var image = new Image();
+                image.onload = function() {
+                    that.canvasImg(image)
+                    that.option.complete(image)
+                }
+                image.src = 'data:image/jpeg;base64,'+ base64Encode(request.responseText);
+            }
+        }
+
+        request.open('GET', that.src, true)
+        request.overrideMimeType('text/plain; charset=x-user-defined')
+        request.send(null)
+
+    }
+
+    // http://www.philten.com/us-xmlhttprequest-image/
+    function base64Encode(inputStr) {
+
+        var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+            outputStr = '',
+            i = 0;
+                   
+        while (i < inputStr.length) {
+            //all three "& 0xff" added below are there to fix a known bug 
+            //with bytes returned by xhr.responseText
+            var byte1 = inputStr.charCodeAt(i++) & 0xff,
+                byte2 = inputStr.charCodeAt(i++) & 0xff,
+                byte3 = inputStr.charCodeAt(i++) & 0xff,
+                
+                enc1 = byte1 >> 2,
+                enc2 = ((byte1 & 3) << 4) | (byte2 >> 4),       
+                enc3,
+                enc4;
+
+                if (isNaN(byte2)) {
+                    enc3 = enc4 = 64;
+                } else {
+                    enc3 = ((byte2 & 15) << 2) | (byte3 >> 6);
+                    if (isNaN(byte3)) {
+                        enc4 = 64;
+                    } else {
+                        enc4 = byte3 & 63;
+                    }
+                }
+            outputStr += b64.charAt(enc1) + b64.charAt(enc2) + b64.charAt(enc3) + b64.charAt(enc4);
+        }         
+        return outputStr
+
+    }
+
+    window.CBFimage = CBFimage;
+
+})(window)
