9bc02c930da88bbd1519edcf33686ef5665f4bde
LoeiFy <loeify@gmail.com>
2016-01-15 15:15:18 +0800

    font family delay load

--- assets/Dreamorphine.js
+++ assets/Dreamorphine.js
@@ -84,6 +84,10 @@ function template(cover) {
 var svg = '<svg x="0px" y="0px" width="36px" height="36px" viewBox="0 0 36 36"><circle fill="none" stroke="#2a76e8" stroke-width="2" cx="18" cy="18" r="16" style="transition: stroke-dashoffset .3s ease;" stroke-dasharray="100 100" stroke-dashoffset="100" transform="rotate(-90 18 18)"></circle></svg>';
 
 $(function($) {
+    setTimeout(function() {
+        $('.static h2').addClass('font')
+    }, 0)
+
     var container = $('#container'), mark = $('#mark'),
         page = 0,   // current page
         target,     // current image
